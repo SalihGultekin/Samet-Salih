2ee59d89275629b7d7be860756d10361857a20f0
samet.ozcelik@abeyis.com <smtab142790@gmail.com>
2023-12-18 14:29:22 +0300

    ilk deneme

--- src/test/java/Iframe.java
+++ src/test/java/Iframe.java
@@ -2,11 +2,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class Iframe {
     static WebDriver driver=new ChromeDriver();
     @Test
     public void Iframe(){
         driver.get("http://www.webdriveruniversity.com");
-// deneme
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     }
 }
