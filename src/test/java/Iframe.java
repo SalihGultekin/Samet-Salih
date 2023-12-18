23640f78166fbd8c94f28bc050ef398ca364b758
samet.ozcelik@abeyis.com <smtab142790@gmail.com>
2023-12-18 12:45:09 +0300

    Ders Notları

---
+++ src/test/java/Iframe.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Iframe {
+    static WebDriver driver=new ChromeDriver();
+    @Test
+    public void Iframe(){
+        driver.get("http://www.webdriveruniversity.com");
+
+    }
+}
